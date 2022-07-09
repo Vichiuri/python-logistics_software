c20e8c785e37a92ad9eba2e09591cb7c54f18d18
vichiuri <chiuri.vic@gmail.com>
2022-07-09 13:22:45 +0300

    password change and reset

--- siliconbiz/urls.py
+++ siliconbiz/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
       path("", include("users.urls")),
-path("accounts/", include("django.contrib.auth.urls")),
+# path("accounts/", include("django.contrib.auth.urls")),
 
 ]
 
--- users/auth.py
+++ users/auth.py
@@ -2,21 +2,30 @@
 from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView, \
      PasswordResetDoneView
 from django.urls import reverse_lazy
-
+from django.shortcuts import render
 class CustomPasswordResetView(PasswordResetView):
-    template_name = 'registration/password_reset_form.html'
+    template_name = 'auth/password_reset_form.html'
 
     
-    success_url = reverse_lazy('users:password_reset_done')
+    success_url = reverse_lazy('password_reset_done')
+
 
 
 class CustomPasswordResetDoneView(PasswordResetDoneView):
-    template_name = 'registration/password_reset_done.html'
+    template_name = 'auth/password_reset_done.html'
 
 
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
-    success_url = reverse_lazy('users:password_reset_complete')
+    template_name = 'auth/password_reset_confirm.html'
+    success_url = reverse_lazy('password_reset_complete')
+    
 
 
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
-    template_name = 'registration/password_reset_complete.html'
+    template_name = 'auth/password_reset_complete.html'
+
+
+
+
+def change_done(request):
+    return render(request, 'auth/password_change_done.html')
